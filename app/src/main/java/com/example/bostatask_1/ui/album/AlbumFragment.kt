77e9dff47201550eb717e97fe826e1344abf8e11
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2023-11-27 21:42:18 +0200

    initial commit

---
+++ app/src/main/java/com/example/bostatask_1/ui/album/AlbumFragment.kt
@@ -0,0 +1,45 @@
+package com.example.bostatask_1.ui.album
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.bostatask_1.databinding.FragmentAlbumBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class AlbumFragment : Fragment() {
+
+    private var _binding: FragmentAlbumBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAlbumBinding.inflate(inflater, container, false)
+        binding.toolbar.title = "hi there"
+        return binding.root
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
